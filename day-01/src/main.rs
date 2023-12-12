cfb5ac0730a7ad7f642b5637075c1a0c5adb6def
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-12 11:36:02 +0100

    feat: finish day 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,22 +2,23 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const test1_file: &str = "test1.txt";
-const test2_file: &str = "test2.txt";
-const part1_file: &str = "part1.txt";
-const part2_file: &str = "part2.txt";
+const RADIX: u32 = 10;
+const TEST1_FILE: &str = "test1.txt";
+const TEST2_FILE: &str = "test2.txt";
+const PART1_FILE: &str = "part1.txt";
+const PART2_FILE: &str = "part2.txt";
 
 fn main() {
     let usage = "Incorrect arguements!\nUsage: day-01 p<n>";
     if let Some(part) = env::args().nth(1) {
         match part.as_str() {
             "p1" => {
-                println!("Reading `{part1_file}`");
-                println!("Sum is {}", part1(part1_file));
+                println!("Reading `{PART1_FILE}`");
+                println!("Sum is {}", part1(PART1_FILE));
             }
             "p2" => {
-                println!("Reading `{part2_file}`");
-                println!("Sum is {}", part2(part2_file));
+                println!("Reading `{PART2_FILE}`");
+                println!("Sum is {}", part2(PART2_FILE));
             }
             _ => eprintln!("{usage}"),
         }
@@ -26,30 +27,33 @@ fn main() {
     }
 }
 
+#[inline]
+fn process_left(line: &String) -> u32 {
+    for c in line.chars() {
+        if c.is_digit(10) {
+            return c.to_digit(10).unwrap();
+        }
+    }
+    unreachable!("there is a digit")
+}
+
+#[inline]
+fn process_right(line: &String) -> u32 {
+    for c in line.chars().rev() {
+        if c.is_digit(10) {
+            return c.to_digit(10).unwrap();
+        }
+    }
+    unreachable!("there is a digit")
+}
+
 fn part1(filename: &str) -> u32 {
     let file = File::open(filename).expect("Should be able to read the value");
     let file = BufReader::new(file);
 
     file.lines().fold(0, |sum, line| {
-        let line = line.unwrap();
-        let mut left = 0;
-        let mut right = 0;
-
-        for c in line.chars() {
-            if c.is_digit(10) {
-                left = c.to_digit(10).unwrap();
-                break;
-            }
-        }
-
-        for c in line.chars().rev() {
-            if c.is_digit(10) {
-                right = c.to_digit(10).unwrap();
-                break;
-            }
-        }
-
-        sum + 10 * left + right
+        let l = line.unwrap();
+        sum + 10 * process_left(&l) + process_right(&l)
     })
 }
 
@@ -57,115 +61,149 @@ fn part2(filename: &str) -> u32 {
     let file = File::open(filename).expect("Should be able to read the value");
     let file = BufReader::new(file);
 
+    let number_parser = NumberParser::new();
+
     file.lines().fold(0, |sum, line| {
-        let line = line.unwrap();
-        let mut left = 0;
-        let mut right = 0;
-
-        println!("{line}");
-
-        for c in line.chars() {
-            if c.is_digit(10) {
-                left = c.to_digit(10).unwrap();
-                break;
-            }
-        }
-
-        for c in line.chars().rev() {
-            if c.is_digit(10) {
-                right = c.to_digit(10).unwrap();
-                break;
-            }
-        }
-        println!("left: {left}, right: {right}");
-
-        sum + 10 * left + right
+        let l = line.unwrap();
+        sum + 10 * number_parser.get_left(&l) + number_parser.get_right(&l)
     })
 }
 
 #[test]
 fn part1_example() {
-    assert_eq!(142, part1(test1_file));
+    assert_eq!(142, part1(TEST1_FILE));
+}
+
+#[test]
+fn part1_puzzle() {
+    assert_eq!(54561, part1(PART1_FILE));
 }
 
 #[test]
 fn part2_example() {
-    assert_eq!(281, part1(test2_file));
+    assert_eq!(281, part2(TEST2_FILE));
 }
 
 #[test]
-fn ce() {
-    let mut root = CharElement::new();
-    root.add_word("zero", 0);
-    root.add_word("one", 1);
-    root.add_word("two", 2);
-    root.add_word("three", 3);
-    root.add_word("four", 4);
-    root.add_word("five", 5);
-    root.add_word("six", 6);
-    root.add_word("seven", 7);
-    root.add_word("eight", 8);
-    root.add_word("nine", 9);
-    for i in root.next {
-        println!("{:?}", i.c)
-    }
-    assert!(false);
+fn part2_puzzle() {
+    assert_eq!(54076, part2(PART2_FILE));
 }
 
-#[derive(Debug, Clone)]
-struct CharElement {
-    c: Option<char>,
-    num: Option<u32>,
-    next: Vec<Self>,
+struct NumberParser {
+    letter: char,
+    digit: Option<u32>,
+    children: Vec<Self>,
 }
 
-impl CharElement {
-    fn new() -> Self {
-        CharElement {
-            c: None,
-            num: None,
-            next: Vec::new(),
+impl NumberParser {
+    fn can_start_digit(&self, letter: char) -> bool {
+        for child in &self.children {
+            if letter == child.letter {
+                return true;
+            }
         }
+        false
     }
 
-    fn get_next(&self, c: char) -> Option<usize> {
-        for (i, element) in self.next.iter().enumerate() {
-            if let Some(ch) = element.c {
-                if ch == c {
-                    return Some(i);
+    fn parse(&self, line: &String, start: usize) -> Option<u32> {
+        let mut chars = line.chars();
+        let index = self.find_child(chars.nth(start).unwrap()).unwrap();
+        let mut node = self.children.get(index).unwrap();
+
+        for c in chars {
+            if let Some(index) = node.find_child(c) {
+                node = node.children.get(index).unwrap();
+                if node.digit.is_some() {
+                    return node.digit;
+                }
+            } else {
+                return None;
+            }
+        }
+        None
+    }
+
+    fn get_left(&self, line: &String) -> u32 {
+        for (index, letter) in line.chars().enumerate() {
+            if letter.is_digit(RADIX) {
+                return letter.to_digit(RADIX).unwrap();
+            } else if self.can_start_digit(letter) {
+                if let Some(digit) = self.parse(line, index) {
+                    return digit;
                 }
             }
         }
+        unreachable!("there must be a digit")
+    }
 
+    fn get_right(&self, line: &String) -> u32 {
+        let line_len = line.len() - 1;
+
+        for (index, letter) in line.chars().rev().enumerate() {
+            if letter.is_digit(RADIX) {
+                return letter.to_digit(RADIX).unwrap();
+            } else if index >= 2 && self.can_start_digit(letter) {
+                if let Some(digit) = self.parse(line, line_len - index) {
+                    return digit;
+                }
+            }
+        }
+        unreachable!("there must be a digit")
+    }
+
+    fn new() -> Self {
+        let mut root = NumberParser::get_new_node('\0');
+
+        root.add_word("zero", 0);
+        root.add_word("one", 1);
+        root.add_word("two", 2);
+        root.add_word("three", 3);
+        root.add_word("four", 4);
+        root.add_word("five", 5);
+        root.add_word("six", 6);
+        root.add_word("seven", 7);
+        root.add_word("eight", 8);
+        root.add_word("nine", 9);
+
+        return root;
+    }
+
+    fn get_new_node(letter: char) -> Self {
+        NumberParser {
+            letter,
+            digit: None,
+            children: Vec::new(),
+        }
+    }
+
+    fn find_child(&self, letter: char) -> Option<usize> {
+        for (index, child) in self.children.iter().enumerate() {
+            if child.letter == letter {
+                return Some(index);
+            }
+        }
         None
     }
 
-    fn add_word(&mut self, word: &str, value: u32) {
+    fn add_word(&mut self, word: &str, digit: u32) {
         let mut node = self;
-        for c in word.chars() {
-            let new_node = CharElement {
-                c: Some(c),
-                num: None,
-                next: Vec::new(),
-            };
 
-            let index = if node.next.is_empty() {
-                node.next.push(new_node);
+        for letter in word.chars() {
+            let new_child = NumberParser::get_new_node(letter);
+
+            let index = if node.children.is_empty() {
+                node.children.push(new_child);
                 0
             } else {
-                if let Some(ii) = node.get_next(c) {
-                    ii
+                if let Some(found_index) = node.find_child(letter) {
+                    found_index
                 } else {
-                    node.next.push(new_node);
-                    node.next.len() - 1
+                    node.children.push(new_child);
+                    node.children.len() - 1
                 }
             };
-            node = node.next.get_mut(index).unwrap();
+            node = node.children.get_mut(index).unwrap();
         }
-        node.num = Some(value);
-    }
-
-    fn parse_num(&mut self, word: &str) -> Option<u32> {
-        None
+        node.digit = Some(digit);
     }
 }
