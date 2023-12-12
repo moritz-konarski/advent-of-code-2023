2036b1211557859694e0a07a925e1e346bc28620
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-12 08:35:09 +0100

    feat: rewrite program with proper form

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,14 +2,114 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+const test1_file: &str = "test1.txt";
+const test2_file: &str = "test2.txt";
+const part1_file: &str = "part1.txt";
+const part2_file: &str = "part2.txt";
+
 fn main() {
-    if let Some(filename) = env::args().nth(1) {
-        println!("Reading `{filename}`");
-        // part1(filename);
-        part2(filename);
+    let usage = "Incorrect arguements!\nUsage: day-01 p<n>";
+    if let Some(part) = env::args().nth(1) {
+        match part.as_str() {
+            "p1" => {
+                println!("Reading `{part1_file}`");
+                println!("Sum is {}", part1(part1_file));
+            }
+            "p2" => {
+                println!("Reading `{part2_file}`");
+                println!("Sum is {}", part2(part2_file));
+            }
+            _ => eprintln!("{usage}"),
+        }
     } else {
-        eprintln!("Missing filename!\nUsage: day-01 <filename>")
+        eprintln!("{usage}");
     }
+}
+
+fn part1(filename: &str) -> u32 {
+    let file = File::open(filename).expect("Should be able to read the value");
+    let file = BufReader::new(file);
+
+    file.lines().fold(0, |sum, line| {
+        let line = line.unwrap();
+        let mut left = 0;
+        let mut right = 0;
+
+        for c in line.chars() {
+            if c.is_digit(10) {
+                left = c.to_digit(10).unwrap();
+                break;
+            }
+        }
+
+        for c in line.chars().rev() {
+            if c.is_digit(10) {
+                right = c.to_digit(10).unwrap();
+                break;
+            }
+        }
+
+        sum + 10 * left + right
+    })
+}
+
+fn part2(filename: &str) -> u32 {
+    let file = File::open(filename).expect("Should be able to read the value");
+    let file = BufReader::new(file);
+
+    file.lines().fold(0, |sum, line| {
+        let line = line.unwrap();
+        let mut left = 0;
+        let mut right = 0;
+
+        println!("{line}");
+
+        for c in line.chars() {
+            if c.is_digit(10) {
+                left = c.to_digit(10).unwrap();
+                break;
+            }
+        }
+
+        for c in line.chars().rev() {
+            if c.is_digit(10) {
+                right = c.to_digit(10).unwrap();
+                break;
+            }
+        }
+        println!("left: {left}, right: {right}");
+
+        sum + 10 * left + right
+    })
+}
+
+#[test]
+fn part1_example() {
+    assert_eq!(142, part1(test1_file));
+}
+
+#[test]
+fn part2_example() {
+    assert_eq!(281, part1(test2_file));
+}
+
+#[test]
+fn ce() {
+    let mut root = CharElement::new();
+    root.add_word("zero", 0);
+    root.add_word("one", 1);
+    root.add_word("two", 2);
+    root.add_word("three", 3);
+    root.add_word("four", 4);
+    root.add_word("five", 5);
+    root.add_word("six", 6);
+    root.add_word("seven", 7);
+    root.add_word("eight", 8);
+    root.add_word("nine", 9);
+    for i in root.next {
+        println!("{:?}", i.c)
+    }
+    assert!(false);
 }
 
 #[derive(Debug, Clone)]
@@ -68,84 +168,4 @@ impl CharElement {
     fn parse_num(&mut self, word: &str) -> Option<u32> {
         None
     }
-}
-
-#[test]
-fn ce() {
-    let mut root = CharElement::new();
-    root.add_word("zero", 0);
-    root.add_word("one", 1);
-    root.add_word("two", 2);
-    root.add_word("three", 3);
-    root.add_word("four", 4);
-    root.add_word("five", 5);
-    root.add_word("six", 6);
-    root.add_word("seven", 7);
-    root.add_word("eight", 8);
-    root.add_word("nine", 9);
-    for i in root.next {
-        println!("{:?}", i.c)
-    }
-    assert!(false);
-}
-
-fn part2(filename: String) {
-    let file = File::open(filename).expect("Should be able to read the value");
-    let file = BufReader::new(file);
-
-    let sum = file.lines().fold(0, |sum, line| {
-        let line = line.unwrap();
-        let mut left = 0;
-        let mut right = 0;
-
-        println!("{line}");
-
-        for c in line.chars() {
-            if c.is_digit(10) {
-                left = c.to_digit(10).unwrap();
-                break;
-            }
-        }
-
-        for c in line.chars().rev() {
-            if c.is_digit(10) {
-                right = c.to_digit(10).unwrap();
-                break;
-            }
-        }
-        println!("left: {left}, right: {right}");
-
-        sum + 10 * left + right
-    });
-
-    println!("The sum is {sum}.");
-}
-
-fn part1(filename: String) {
-    let file = File::open(filename).expect("Should be able to read the value");
-    let file = BufReader::new(file);
-
-    let sum = file.lines().fold(0, |sum, line| {
-        let line = line.unwrap();
-        let mut left = 0;
-        let mut right = 0;
-
-        for c in line.chars() {
-            if c.is_digit(10) {
-                left = c.to_digit(10).unwrap();
-                break;
-            }
-        }
-
-        for c in line.chars().rev() {
-            if c.is_digit(10) {
-                right = c.to_digit(10).unwrap();
-                break;
-            }
-        }
-
-        sum + 10 * left + right
-    });
-
-    println!("The sum is {sum}.");
 }
