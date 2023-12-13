336cbdf5f10566193c40539fc4d3fc11e39ba689
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-13 23:08:15 +0100

    feat: finish part 1

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -27,67 +27,70 @@ fn main() {
     }
 }
 
-fn part1(filename: &str) -> u32 {
-    let file = File::open(filename).expect("Should be able to read the file");
-    let file = BufReader::new(file);
+fn process_previous_line(line: &(Vec<u8>, Vec<bool>), line_len: usize) -> u32 {
+    let mut num_counts = false;
+    let mut sum = 0;
+    let mut i = 0;
 
-    // get tuples for lines
-    let mut previous_line: (String, Vec<bool>) = (String::new(), Vec::new());
-    let mut current_line: (String, Vec<bool>) = (String::new(), Vec::new());
-    let mut next_line: Vec<bool> = Vec::new();
+    while i < line_len {
+        if !(ZERO_AS_BYTE..=NINE_AS_BYTE).contains(&line.0[i]) {
+            i += 1;
+            continue;
+        }
 
-    // iter over all lines
-    for line in file.lines() {
-        current_line.0 = line.unwrap();
+        let start = i;
+        while i < line_len && line.0[i] >= ZERO_AS_BYTE && line.0[i] <= NINE_AS_BYTE {
+            num_counts |= line.1[i];
+            i += 1;
+        }
+        let end = i;
 
-        // TODO: remove
-        println!("{:?}", current_line.0);
-
-        let mut index = 0;
-        while index < current_line.0.len() {
-            match current_line.0.as_bytes()[index] {
-                PERIOD_AS_BYTE => { /* we ignore periods */ }
-                ZERO_AS_BYTE..=NINE_AS_BYTE => {
-                    let start = index;
-                    index += 1;
-                    // while we have digits, increment column
-                    while index < current_line.0.len()
-                        && current_line.0.as_bytes()[index] >= ZERO_AS_BYTE
-                        && current_line.0.as_bytes()[index] <= NINE_AS_BYTE
-                    {
-                        index += 1;
-                    }
-                    let end = index;
-
-                    // extract the number
-                    let mut num = 0;
-                    for (count, index) in (start..end).into_iter().rev().enumerate() {
-                        num += 10_u32.pow(count as u32)
-                            * (current_line.0.as_bytes()[index] - ZERO_AS_BYTE) as u32;
-                    }
-
-                    // TODO: remove
-                    println!("{num}");
-                }
-                _ => {
-                    // everything else is treated as a symbol
-                    // TODO: remove
-                    // each symbol creates 3 three-ranges of legal nums above, below and on the same line
-                    println!(
-                        "{:?}",
-                        char::from_u32(current_line.0.as_bytes()[index] as u32)
-                            .expect("cannot fail")
-                    );
-                }
+        if num_counts {
+            num_counts = false;
+            for (count, ii) in (start..end).into_iter().rev().enumerate() {
+                sum += 10_u32.pow(count as u32) * (line.0[ii] - ZERO_AS_BYTE) as u32;
             }
-
-            index += 1;
-            previous_line = current_line.clone();
-            current_line.1 = next_line.clone();
-            next_line = Vec::with_capacity(next_line.len());
         }
     }
-    0
+
+    return sum;
+}
+
+fn part1(filename: &str) -> u32 {
+    // this is really dirty, but it comes in handy
+    let file = BufReader::new(File::open(filename).expect("Should be able to read the file"));
+    let line_len = file.lines().next().unwrap().unwrap().len();
+    let file = BufReader::new(File::open(filename).expect("Should be able to read the file"));
+
+    let mut previous_line = (vec![0; line_len], vec![false; line_len]);
+    let mut current_line = (vec![0; line_len], vec![false; line_len]);
+    let mut next_line = vec![false; line_len];
+
+    let mut sum = 0;
+
+    for line in file.lines() {
+        current_line.0 = line.unwrap().as_bytes().to_vec();
+
+        for (i, c) in current_line.0.iter().enumerate() {
+            if *c == PERIOD_AS_BYTE || (ZERO_AS_BYTE..=NINE_AS_BYTE).contains(c) {
+                continue;
+            }
+
+            for ii in 0.max(i - 1)..line_len.min(i + 2) {
+                previous_line.1[ii] |= true;
+                current_line.1[ii] |= true;
+                next_line[ii] |= true;
+            }
+        }
+
+        sum += process_previous_line(&previous_line, line_len);
+
+        previous_line = current_line.to_owned();
+        current_line.1 = next_line;
+        next_line = vec![false; line_len];
+    }
+
+    sum + process_previous_line(&previous_line, line_len)
 }
 
 fn part2(filename: &str) -> u32 {
@@ -109,10 +112,10 @@ fn part1_example() {
     assert_eq!(4361, part1("test1.txt"));
 }
 
-// #[test]
-// fn part1_puzzle() {
-//     assert_eq!(2545, part1(PART1_FILE));
-// }
+#[test]
+fn part1_puzzle() {
+    assert_eq!(550064, part1(PART1_FILE));
+}
 
 // #[test]
 // fn part2_example() {
