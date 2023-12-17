0b429d25444f8a2027216b8374835b589ce2fcfe
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-17 20:36:44 +0100

    feat: setup day 6

---
+++ day-06/src/main.rs
@@ -0,0 +1,59 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+
+const PART1_FILE: &str = "part1.txt";
+const PART2_FILE: &str = "part2.txt";
+
+fn main() {
+    let usage = "Incorrect arguements!\nUsage: day-06 p<n>";
+    if let Some(part) = env::args().nth(1) {
+        match part.as_str() {
+            "p1" => {
+                println!("Reading `{PART1_FILE}`");
+                println!("Sum is {}", part1(PART1_FILE));
+            }
+            "p2" => {
+                println!("Reading `{PART2_FILE}`");
+                println!("Sum is {}", part2(PART2_FILE));
+            }
+            _ => eprintln!("{usage}"),
+        }
+    } else {
+        eprintln!("{usage}");
+    }
+}
+
+fn part1(filename: &str) -> usize {
+    let file = File::open(filename).expect("Should be able to read the file");
+    let file = BufReader::new(file);
+
+    0
+}
+
+fn part2(filename: &str) -> usize {
+    let file = File::open(filename).expect("Should be able to read the file");
+    let file = BufReader::new(file);
+
+    0
+}
+
+#[test]
+fn part1_example() {
+    assert_eq!(35, part1("test1.txt"));
+}
+
+// #[test]
+// fn part1_puzzle() {
+//     assert_eq!(227653707, part1(PART1_FILE));
+// }
+
+// #[test]
+// fn part2_example() {
+//     assert_eq!(46, part2("test2.txt"));
+// }
+
+// #[test]
+// fn part2_puzzle() {
+//     assert_eq!(78775051, part2(PART2_FILE));
+// }
