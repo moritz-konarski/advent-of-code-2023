b1810017d10a398ea9476be06b38bcd9498a7a8a
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-17 21:09:05 +0100

    feat: solve day 6

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 
 const PART1_FILE: &str = "part1.txt";
 const PART2_FILE: &str = "part2.txt";
@@ -24,36 +24,77 @@ fn main() {
     }
 }
 
+fn get_nums(line: &str) -> Vec<usize> {
+    let (_, times) = line.split_once(':').unwrap();
+    times
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn get_nums_merged(line: &str) -> usize {
+    let (_, times) = line.split_once(':').unwrap();
+    let times: String = times.split_ascii_whitespace().collect();
+    times.parse().unwrap()
+}
+
 fn part1(filename: &str) -> usize {
     let file = File::open(filename).expect("Should be able to read the file");
     let file = BufReader::new(file);
 
-    0
+    let mut lines = file.lines();
+    let times = get_nums(&lines.next().unwrap().unwrap());
+    let distances = get_nums(&lines.next().unwrap().unwrap());
+
+    times
+        .iter()
+        .zip(distances)
+        .fold(1, |product, (time, distance)| {
+            let options = (1..*time).into_iter().fold(0, |count, t| {
+                if t * (time - t) > distance {
+                    count + 1
+                } else {
+                    count
+                }
+            });
+
+            product * options
+        })
 }
 
 fn part2(filename: &str) -> usize {
     let file = File::open(filename).expect("Should be able to read the file");
     let file = BufReader::new(file);
 
-    0
+    let mut lines = file.lines();
+    let time = get_nums_merged(&lines.next().unwrap().unwrap());
+    let distance = get_nums_merged(&lines.next().unwrap().unwrap());
+
+    (1..time).into_iter().fold(0, |count, t| {
+        if t * (time - t) > distance {
+            count + 1
+        } else {
+            count
+        }
+    })
 }
 
 #[test]
 fn part1_example() {
-    assert_eq!(35, part1("test1.txt"));
+    assert_eq!(288, part1("test1.txt"));
 }
 
-// #[test]
-// fn part1_puzzle() {
-//     assert_eq!(227653707, part1(PART1_FILE));
-// }
+#[test]
+fn part1_puzzle() {
+    assert_eq!(293046, part1(PART1_FILE));
+}
 
-// #[test]
-// fn part2_example() {
-//     assert_eq!(46, part2("test2.txt"));
-// }
+#[test]
+fn part2_example() {
+    assert_eq!(71503, part2("test2.txt"));
+}
 
-// #[test]
-// fn part2_puzzle() {
-//     assert_eq!(78775051, part2(PART2_FILE));
-// }
+#[test]
+fn part2_puzzle() {
+    assert_eq!(35150181, part2(PART2_FILE));
+}
