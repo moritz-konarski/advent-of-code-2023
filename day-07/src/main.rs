e36d25aef2e608c742f1f5cfd660af6810233226
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-18 08:00:00 +0100

    feat: start implementation

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -24,9 +25,61 @@ fn main() {
     }
 }
 
+const CARDS: [(char, usize); 13] = [
+    ('A', 12),
+    ('K', 11),
+    ('Q', 10),
+    ('J', 9),
+    ('T', 8),
+    ('9', 7),
+    ('8', 6),
+    ('7', 5),
+    ('6', 4),
+    ('5', 3),
+    ('4', 2),
+    ('3', 1),
+    ('2', 0),
+];
+
+struct Hand {
+    cards: [usize; 5],
+    bid: usize,
+}
+
+impl Hand {
+    fn new(line: &str, map: &HashMap<char, usize>) -> Self {
+        let (hand, bid) = line.split_once(' ').unwrap();
+        let bid = bid;
+
+        Self {
+            cards: [0; 5],
+            bid: 0,
+        }
+    }
+}
+
 fn part1(filename: &str) -> usize {
     let file = File::open(filename).expect("Should be able to read the file");
     let file = BufReader::new(file);
+
+    let map = HashMap::from(CARDS);
+    let mut hands = Vec::new();
+
+    for line in file.lines() {
+        let line = line.unwrap();
+        hands.push(Hand::new(&line, &map));
+    }
+
+    // poker hands
+    // high card: 5 unique cards
+    // pair: 4 unique cards
+    // two pair: 3 unique cards
+    // triplet: 3 unique cards
+    // full house: 2 unique cards
+    // quadruplet: 2 unique cards
+    // quintuples: 1 unique cards
+
+    // then order then by relative card strength
 
     0
 }
