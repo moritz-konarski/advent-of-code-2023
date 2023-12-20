8bdf0f663d44d21c2c40b2138cc99d83ec8b6e86
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-20 14:03:06 +0100

    feat: implement very simple traversal

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,15 +1,11 @@
 use std::env;
 
-use alloc::collections;
-
 const PART1_FILE: &str = "part1.txt";
 const PART2_FILE: &str = "part2.txt";
-const START_CHAR: char = 'S';
-const WEST: Point = Point::new(0, -1);
-const EAST: Point = Point::new(0, 1);
-const NORTH: Point = Point::new(-1, 0);
-const SOUTH: Point = Point::new(1, 0);
-const DX_DY_VEC: [Point; 4] = [WEST, EAST, NORTH, SOUTH];
+const WEST: Point = Point { row: 0, col: -1 };
+const EAST: Point = Point { row: 0, col: 1 };
+const NORTH: Point = Point { row: -1, col: 0 };
+const SOUTH: Point = Point { row: 1, col: 0 };
 
 fn main() {
     let usage = "Incorrect arguements!\nUsage: day-10 p<n>";
@@ -30,15 +26,18 @@ fn main() {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 struct Point {
     row: isize,
     col: isize,
 }
 
 impl Point {
-    fn new(row: isize, col: isize) -> Self {
-        Self { row, col }
+    fn new(row: usize, col: usize) -> Self {
+        Self {
+            row: row as isize,
+            col: col as isize,
+        }
     }
 
     fn row(&self) -> usize {
@@ -83,78 +82,82 @@ impl CharArray {
     }
 
     fn find_start(&self) -> Point {
-        let start_row = self
+        let row = self
             .chars
             .iter()
-            .position(|row| row.iter().position(|c| *c == START_CHAR).is_some())
+            .position(|row| row.iter().position(|c| *c == 'S').is_some())
             .unwrap();
 
-        let start_col = self.chars[start_row]
-            .iter()
-            .position(|c| *c == START_CHAR)
-            .unwrap();
+        let col = self.chars[row].iter().position(|c| *c == 'S').unwrap();
 
-        Point::new(start_row as isize, start_col as isize)
+        Point::new(row, col)
     }
 
     fn get(&self, point: &Point) -> Option<&char> {
         if !point.can_be_index() {
             return None;
         }
-
-        if let Some(row) = self.chars.get(point.row()) {
-            row.get(point.col())
-        } else {
-            None
-        }
+        self.chars
+            .get(point.row())
+            .and_then(|row| row.get(point.col()))
     }
 
     fn get_pipe(&self, point: &Point) -> Option<&char> {
-        if let Some(char) = self.get(point) {
-            match char {
-                '|' | '-' | 'L' | 'J' | '7' | 'F' => Some(char),
-                _ => None,
-            }
+        self.get(point).and_then(|char| match char {
+            '|' | '-' | 'L' | 'J' | '7' | 'F' | 'S' => Some(char),
+            _ => None,
+        })
+    }
+
+    fn get_directions(pipe: &char) -> Vec<Point> {
+        match pipe {
+            'S' => vec![NORTH, SOUTH, EAST, WEST],
+            '|' => vec![NORTH, SOUTH],
+            '-' => vec![EAST, WEST],
+            'L' => vec![NORTH, EAST],
+            'J' => vec![NORTH, WEST],
+            '7' => vec![SOUTH, WEST],
+            'F' => vec![SOUTH, EAST],
+            _ => unreachable!("no other chars allowed"),
+        }
+    }
+
+    fn is_connected(&self, start_point: &Point, pipe: &char, other_point: &Point) -> Option<Point> {
+        if let Some(other_pipe) = self.get_pipe(other_point) {
+            let other_directions = CharArray::get_directions(other_pipe);
+
+            None
         } else {
             None
         }
     }
 
-    fn follow_connection(&self, point: &Point, offset: &Point, pipe: &char) -> Option<Point> {
-        match (*pipe, *offset) {
-            ('|', NORTH)
-            | ('|', SOUTH)
-            | ('-', WEST)
-            | ('-', EAST)
-            | ('L', SOUTH)
-            | ('L', WEST)
-            | ('J', EAST)
-            | ('J', SOUTH)
-            | ('7', NORTH)
-            | ('7', EAST)
-            | ('F', NORTH)
-            | ('F', WEST) => Some(point.move_by(offset)),
-            _ => None,
+    fn find_connected_points(&self, point: &Point) -> Vec<Point> {
+        let mut connected_points = vec![];
+        if let Some(pipe) = self.get_pipe(point) {
+            for direction in CharArray::get_directions(pipe) {
+                let other_point = point.move_by(&direction);
+                if let Some(other_pipe) = self.get_pipe(&other_point) {
+                    let other_directions = CharArray::get_directions(other_pipe);
+                    if other_directions
+                        .iter()
+                        .find(|dir| other_point.move_by(dir) == *point)
+                        .is_some()
+                    {
+                        connected_points.push(other_point);
+                    }
+                }
+            }
         }
+        connected_points
     }
 
-    fn find_connected_points(&self, point: &Point) -> Option<Vec<Point>> {
-        let adjacent_pipes: Vec<_> = DX_DY_VEC
+    fn get_next(&self, point: &Point) -> Point {
+        *self
+            .find_connected_points(point)
             .iter()
-            .filter_map(|offset| {
-                if let Some(pipe) = self.get_pipe(&point.move_by(&offset)) {
-                    self.follow_connection(point, offset, pipe)
-                } else {
-                    None
-                }
-            })
-            .collect();
-
-        if adjacent_pipes.is_empty() {
-            return None;
-        } else {
-            Some(adjacent_pipes)
-        }
+            .find(|other| *other != point)
+            .unwrap()
     }
 }
 
@@ -165,16 +168,31 @@ fn part1(filename: &str) -> usize {
     let char_array = CharArray::new(&file);
 
     let start_point = char_array.find_start();
+    println!("{start_point:?}");
+    let connected_points = char_array.find_connected_points(&start_point);
+    let mut point_1 = connected_points[0];
+    let mut point_2 = connected_points[1];
 
-    let a = 
+    println!("{point_1:?} - {point_2:?}");
 
-    // load in complete file
-    // find S
-    // from S, use two-pointer method to traverse the loop
-    // count the steps for each pointer
-    // when they meet, return that number
+    point_1 = char_array.get_next(&point_1);
+    point_2 = char_array.get_next(&point_2);
 
-    0
+    // TODO: implement something to make sure we don't move backwards
+
+    println!("{point_1:?} - {point_2:?}");
+
+    let mut steps = 0;
+    while point_1 != point_2 {
+        point_1 = char_array.get_next(&point_1);
+        point_2 = char_array.get_next(&point_2);
+
+        println!("{point_1:?} - {point_2:?}");
+        steps += 1;
+        break;
+    }
+
+    steps
 }
 
 fn part2(filename: &str) -> usize {
@@ -183,10 +201,10 @@ fn part2(filename: &str) -> usize {
     0
 }
 
-#[test]
-fn part1_example() {
-    assert_eq!(8, part1("test1.txt"));
-}
+// #[test]
+// fn part1_example() {
+//     assert_eq!(8, part1("test1.txt"));
+// }
 
 #[test]
 fn part1_example1() {
