2204a1634a9996c17bbb91125e83c2092f1fd4ca
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-25 06:59:40 +0100

    feat: start tilting

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -2,6 +2,9 @@ use std::env;
 
 const PART1_FILE: &str = "part1.txt";
 const PART2_FILE: &str = "part2.txt";
+const ROUND: u8 = b'O';
+const CUBE: u8 = b'#';
+const EMPTY: u8 = b'.';
 
 fn main() {
     let usage = "Incorrect arguements!\nUsage: day-14 p<n>";
@@ -22,19 +25,57 @@ fn main() {
     }
 }
 
-fn tilt_north(lines: &mut [&[u8]]) ->
+fn tilt_north(lines: &mut [Vec<u8>]) {
+    let mut empty_index: Option<usize>;
+
+    for col in 0..lines[0].len() {
+        empty_index = None;
+
+        for row in 0..lines.len() {
+            // TODO: make 2nd match where empty_index is incremented separately
+
+            match lines[row][col] {
+                EMPTY => {
+                    if empty_index.is_none() {
+                        empty_index = Some(row);
+                    }
+                }
+                CUBE => {
+                    empty_index = None;
+                }
+                ROUND => {
+                    if let Some(index) = empty_index {
+                        lines[row][col] = EMPTY;
+                        lines[index][col] = ROUND;
+
+                        empty_index = Some(row);
+                    }
+                }
+                _ => unreachable!("no other shapes"),
+            }
+        }
+    }
+}
 
 fn part1(filename: &str) -> usize {
-    let lines: Vec<_> = std::fs::read_to_string(filename)
-        .unwrap()
+    let file = std::fs::read_to_string(filename).unwrap();
+    let mut lines: Vec<_> = file
         .split_ascii_whitespace()
-        .map(|line| line.as_bytes())
+        .map(|line| line.as_bytes().to_vec())
         .collect();
 
-    // read in the data
-    // tilt all the rocks north
+    lines
+        .iter()
+        .for_each(|l| println!("{}", String::from_utf8(l.clone()).unwrap()));
+    println!();
+
+    tilt_north(&mut lines);
     // O moves, . is empty, # does not move
     // calculate load for O as len() - row for each O
+
+    lines
+        .iter()
+        .for_each(|l| println!("{}", String::from_utf8(l.clone()).unwrap()));
 
     0
 }
