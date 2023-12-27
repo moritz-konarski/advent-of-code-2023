4bda2616bf40d0911cba6ec95c07d34fadfac7ae
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-27 08:31:59 +0100

    feat: use transpose

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use transpose;
 
 const PART1_FILE: &str = "part1.txt";
 const PART2_FILE: &str = "part2.txt";
@@ -23,6 +24,93 @@ fn main() {
         }
     } else {
         eprintln!("{usage}");
+    }
+}
+
+struct Array {
+    data: Vec<u8>,
+    width: usize,
+    height: usize,
+}
+
+impl Array {
+    fn new(file: &String) -> Self {
+        let data: Vec<_> = file
+            .split_ascii_whitespace()
+            .map(|line| line.as_bytes().to_vec())
+            .collect();
+
+        let width = data[0].len();
+        let height = data.len();
+
+        let data = data.iter().flat_map(|e| e.clone()).collect();
+
+        Self {
+            data,
+            width,
+            height,
+        }
+    }
+
+    fn tilt_north(&mut self) {
+        let mut empty_tracker: Vec<_> = (0..self.width).map(|_| None).collect();
+
+        for row in 0..self.height {
+            for (col, empty_index) in empty_tracker.iter_mut().enumerate() {
+                match self.get(row, col) {
+                    EMPTY => {
+                        if empty_index.is_none() {
+                            *empty_index = Some(row);
+                        }
+                    }
+                    CUBE => *empty_index = None,
+                    ROUND => {
+                        if let Some(row_index) = empty_index {
+                            self.set(row, col, EMPTY);
+                            self.set(*row_index, col, ROUND);
+
+                            *empty_index =
+                                (*row_index + 1..row + 1).find(|i| self.get(*i, col) == EMPTY);
+                        }
+                    }
+                    _ => unreachable!("impossible symbol {:?}", self.get(row, col)),
+                }
+            }
+        }
+    }
+
+    fn get(&self, row: usize, col: usize) -> u8 {
+        self.data[self.width * row + col]
+    }
+
+    fn set(&mut self, row: usize, col: usize, value: u8) {
+        self.data[self.width * row + col] = value;
+    }
+
+    fn count_rounds(&self) -> usize {
+        let mut sum = 0;
+        let mut round_count;
+
+        for row in (0..self.height).rev() {
+            round_count = 0;
+            for col in 0..self.width {
+                if self.get(row, col) == ROUND {
+                    round_count += 1;
+                }
+            }
+            sum += (row + 1) * round_count;
+        }
+
+        sum
+    }
+
+    fn rotate_counter_clockwise(&mut self) {
+        let mut scratch = vec![0; self.width.max(self.height)];
+        transpose::transpose_inplace(&mut self.data, &mut scratch, self.width, self.height);
+
+        let t = self.height;
+        self.height = self.width;
+        self.width = t;
     }
 }
 
@@ -146,14 +234,20 @@ fn count_rounds(lines: &[Vec<u8>]) -> usize {
 
 fn part1(filename: &str) -> usize {
     let file = std::fs::read_to_string(filename).unwrap();
-    let mut lines: Vec<_> = file
-        .split_ascii_whitespace()
-        .map(|line| line.as_bytes().to_vec())
-        .collect();
 
-    tilt_north(&mut lines);
+    let mut a = Array::new(&file);
 
-    count_rounds(&lines)
+    a.tilt_north();
+
+    a.count_rounds()
+    // let mut lines: Vec<_> = file
+    //     .split_ascii_whitespace()
+    //     .map(|line| line.as_bytes().to_vec())
+    //     .collect();
+
+    // tilt_north(&mut lines);
+
+    // count_rounds(&lines)
 }
 
 fn part2(filename: &str) -> usize {
@@ -193,10 +287,10 @@ fn part1_puzzle() {
     assert_eq!(108813, part1(PART1_FILE));
 }
 
-#[test]
-fn part2_example() {
-    assert_eq!(64, part2("test2.txt"));
-}
+// #[test]
+// fn part2_example() {
+//     assert_eq!(64, part2("test2.txt"));
+// }
 
 // #[test]
 // fn part2_puzzle() {
