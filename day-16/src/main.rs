be74a8eb8b02d5479764ee4a6525279773e811ed
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-29 13:40:18 +0100

    feat: solve part 1

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,11 +1,17 @@
-use std::env;
+use std::{collections::HashSet, env};
 
 const PART1_FILE: &str = "part1.txt";
 const PART2_FILE: &str = "part2.txt";
-const RIGHT: Vector = Vector { dx: 1, dy: 0 };
-const LEFT: Vector = Vector { dx: -1, dy: 0 };
-const UP: Vector = Vector { dx: 0, dy: -1 };
-const DOWN: Vector = Vector { dx: 0, dy: 1 };
+const RIGHT: Vector = Vector { d_row: 0, d_col: 1 };
+const LEFT: Vector = Vector {
+    d_row: 0,
+    d_col: -1,
+};
+const UP: Vector = Vector {
+    d_row: -1,
+    d_col: 0,
+};
+const DOWN: Vector = Vector { d_row: 1, d_col: 0 };
 
 fn main() {
     let usage = "Incorrect arguements!\nUsage: day-16 p<n>";
@@ -26,21 +32,36 @@ fn main() {
     }
 }
 
+enum Turn {
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Vector {
-    dx: isize,
-    dy: isize,
+    d_row: isize,
+    d_col: isize,
 }
 
 impl Vector {
-    fn x(&self) -> usize {
-        self.dx.max(0) as usize
+    fn turn(&self, new_dir: Turn) -> Self {
+        match new_dir {
+            Turn::Left => match *self {
+                LEFT => DOWN,
+                RIGHT => UP,
+                UP => LEFT,
+                DOWN => RIGHT,
+                _ => unreachable!("illegal vector value {:?}", self),
+            },
+            Turn::Right => match *self {
+                LEFT => UP,
+                RIGHT => DOWN,
+                UP => RIGHT,
+                DOWN => LEFT,
+                _ => unreachable!("illegal vector value {:?}", self),
+            },
+        }
     }
-
-    fn y(&self) -> usize {
-        self.dy.max(0) as usize
-    }
-
-    // TODO: reflections
 }
 
 impl std::ops::Add for Vector {
@@ -48,24 +69,118 @@ impl std::ops::Add for Vector {
 
     fn add(self, other: Self) -> Self {
         Self {
-            dx: self.dx + other.dx,
-            dy: self.dy + other.dy,
+            d_row: self.d_row + other.d_row,
+            d_col: self.d_col + other.d_col,
         }
     }
 }
 
+#[derive(Clone, Copy)]
 struct Beam {
     direction: Vector,
+    position: Vector,
 }
 
 impl Beam {
-    fn split(self) -> (Self, Self) {}
+    fn new() -> Self {
+        Self {
+            direction: RIGHT,
+            position: Vector {
+                d_row: 0,
+                d_col: -1,
+            },
+        }
+    }
+
+    fn advance(&mut self) {
+        self.position = self.position + self.direction;
+    }
+
+    fn is_negative(&self) -> bool {
+        self.position.d_row < 0 || self.position.d_col < 0
+    }
+
+    fn row(&self) -> usize {
+        self.position.d_row as usize
+    }
+
+    fn col(&self) -> usize {
+        self.position.d_col as usize
+    }
+
+    fn turn(self, new_dir: Turn) -> Option<Vec<Self>> {
+        let new_self = Self {
+            direction: self.direction.turn(new_dir),
+            position: self.position,
+        };
+
+        Some(vec![new_self])
+    }
+
+    fn split(self) -> Option<Vec<Self>> {
+        let left = Beam {
+            direction: self.direction.turn(Turn::Left),
+            position: self.position,
+        };
+        let right = Beam {
+            direction: self.direction.turn(Turn::Right),
+            position: self.position,
+        };
+
+        Some(vec![left, right])
+    }
 }
 
 fn part1(filename: &str) -> usize {
     let file = std::fs::read_to_string(filename).unwrap();
+    let contraption: Vec<Vec<_>> = file
+        .split_ascii_whitespace()
+        .map(|line| line.chars().collect())
+        .collect();
+    let (row_count, col_count) = (contraption.len(), contraption[0].len());
 
-    0
+    let mut beams = vec![Beam::new()];
+    let mut visited_tiles = HashSet::new();
+
+    let mut last_visited_counts = vec![0; 10];
+
+    while !beams.is_empty() {
+        beams = beams
+            .iter_mut()
+            .filter_map(|beam| {
+                beam.advance();
+
+                // remove a beam if it is out of bounds
+                if beam.is_negative() || beam.row() >= row_count || beam.col() >= col_count {
+                    return None;
+                }
+
+                // track the tile as visited
+                visited_tiles.insert(beam.position);
+
+                // interact with environment
+                match (contraption[beam.row()][beam.col()], beam.direction) {
+                    ('|', LEFT) | ('|', RIGHT) => beam.split(),
+                    ('-', UP) | ('-', DOWN) => beam.split(),
+                    ('/', LEFT) | ('/', RIGHT) => beam.turn(Turn::Left),
+                    ('/', UP) | ('/', DOWN) => beam.turn(Turn::Right),
+                    ('\\', LEFT) | ('\\', RIGHT) => beam.turn(Turn::Right),
+                    ('\\', UP) | ('\\', DOWN) => beam.turn(Turn::Left),
+                    (_, _) => Some(vec![*beam]),
+                }
+            })
+            .flatten()
+            .collect();
+
+        let visited_count = visited_tiles.len();
+        if last_visited_counts.iter().all(|e| *e == visited_count) {
+            break;
+        }
+        last_visited_counts.remove(0);
+        last_visited_counts.push(visited_count);
+    }
+
+    visited_tiles.len()
 }
 
 fn part2(filename: &str) -> usize {
@@ -86,10 +201,10 @@ fn part1_example() {
     assert_eq!(46, part1("test1.txt"));
 }
 
-// #[test]
-// fn part1_puzzle() {
-//     assert_eq!(504036, part1(PART1_FILE));
-// }
+#[test]
+fn part1_puzzle() {
+    assert_eq!(7210, part1(PART1_FILE));
+}
 
 // #[test]
 // fn part2_example() {
