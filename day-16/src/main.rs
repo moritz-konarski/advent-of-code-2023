7d259583f54740ca9f6c0d6193af566ecbeadf0f
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-29 10:16:45 +0100

    feat: start impl

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -2,6 +2,10 @@ use std::env;
 
 const PART1_FILE: &str = "part1.txt";
 const PART2_FILE: &str = "part2.txt";
+const RIGHT: Vector = Vector { dx: 1, dy: 0 };
+const LEFT: Vector = Vector { dx: -1, dy: 0 };
+const UP: Vector = Vector { dx: 0, dy: -1 };
+const DOWN: Vector = Vector { dx: 0, dy: 1 };
 
 fn main() {
     let usage = "Incorrect arguements!\nUsage: day-16 p<n>";
@@ -22,6 +26,42 @@ fn main() {
     }
 }
 
+struct Vector {
+    dx: isize,
+    dy: isize,
+}
+
+impl Vector {
+    fn x(&self) -> usize {
+        self.dx.max(0) as usize
+    }
+
+    fn y(&self) -> usize {
+        self.dy.max(0) as usize
+    }
+
+    // TODO: reflections
+}
+
+impl std::ops::Add for Vector {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            dx: self.dx + other.dx,
+            dy: self.dy + other.dy,
+        }
+    }
+}
+
+struct Beam {
+    direction: Vector,
+}
+
+impl Beam {
+    fn split(self) -> (Self, Self) {}
+}
+
 fn part1(filename: &str) -> usize {
     let file = std::fs::read_to_string(filename).unwrap();
 
@@ -31,12 +71,19 @@ fn part1(filename: &str) -> usize {
 fn part2(filename: &str) -> usize {
     let file = std::fs::read_to_string(filename).unwrap();
 
+    // beam enters (0, 0) coming from (-1, 0)
+    // then bounces around the chamber
+    // ignores .
+    // / and \ are mirrors that reflect the beam 90 degrees
+    // flat end of | or - -> two beams that move to where the ends of the splitters are pointing
+    // energized tile: at least one beam on top of it
+
     0
 }
 
 #[test]
 fn part1_example() {
-    assert_eq!(1320, part1("test1.txt"));
+    assert_eq!(46, part1("test1.txt"));
 }
 
 // #[test]
