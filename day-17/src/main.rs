1b361871df591c4298d2cf0c5bd5cd9a1b85fa61
Moritz Konarski <moritz.konarski@gmail.com>
2023-12-31 13:08:10 +0100

    feat: optimize part 1

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,20 +1,14 @@
 use std::{
-    collections::{btree_map::Entry, HashMap, HashSet},
+    collections::{HashMap, HashSet},
     env,
 };
 
 const PART1_FILE: &str = "part1.txt";
 const PART2_FILE: &str = "part2.txt";
-const RIGHT: Vector = Vector { d_row: 0, d_col: 1 };
-const LEFT: Vector = Vector {
-    d_row: 0,
-    d_col: -1,
-};
-const UP: Vector = Vector {
-    d_row: -1,
-    d_col: 0,
-};
-const DOWN: Vector = Vector { d_row: 1, d_col: 0 };
+const RIGHT: Vector = Vector { dr: 0, dc: 1 };
+const LEFT: Vector = Vector { dr: 0, dc: -1 };
+const UP: Vector = Vector { dr: -1, dc: 0 };
+const DOWN: Vector = Vector { dr: 1, dc: 0 };
 
 fn main() {
     let usage = "Incorrect arguements!\nUsage: day-17 p<n>";
@@ -22,8 +16,7 @@ fn main() {
         match part.as_str() {
             "p1" => {
                 println!("Reading `{PART1_FILE}`");
-                println!("Sum is {}", part1("test1.txt"));
-                // println!("Sum is {}", part1(PART1_FILE));
+                println!("Sum is {}", part1(PART1_FILE));
             }
             "p2" => {
                 println!("Reading `{PART2_FILE}`");
@@ -43,8 +36,8 @@ enum Turn {
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Vector {
-    d_row: isize,
-    d_col: isize,
+    dr: isize,
+    dc: isize,
 }
 
 impl Vector {
@@ -73,8 +66,8 @@ impl std::ops::Add for Vector {
 
     fn add(self, other: Self) -> Self {
         Self {
-            d_row: self.d_row + other.d_row,
-            d_col: self.d_col + other.d_col,
+            dr: self.dr + other.dr,
+            dc: self.dc + other.dc,
         }
     }
 }
@@ -83,179 +76,213 @@ impl std::ops::Add for Vector {
 struct Path {
     direction: Vector,
     position: Vector,
-    visited_positions: Vec<Vector>,
+    visited_positions: HashSet<Vector>,
     has_cycle: bool,
     straight_count: usize,
     length: usize,
 }
 
 impl Path {
-    fn new(start_pos: Vector, start_dir: Vector) -> Self {
+    fn new(direction: Vector, position: Vector) -> Self {
         Self {
-            direction: start_dir,
-            position: start_pos,
-            visited_positions: vec![start_pos],
+            direction,
+            position,
+            visited_positions: HashSet::from([position]),
             has_cycle: false,
             straight_count: 0,
             length: 0,
         }
     }
 
-    fn advance(&mut self) {
-        let new_pos = self.position + self.direction;
-        self.has_cycle |= self.visited_positions.contains(&new_pos);
-        self.visited_positions.push(new_pos);
-        self.position = new_pos;
-        self.straight_count += 1;
-    }
-
-    fn in_bounds(&self) -> bool {
-        self.position.d_row >= 0 && self.position.d_col >= 0
-    }
-
-    fn row(&self) -> usize {
-        self.position.d_row as usize
-    }
-
-    fn col(&self) -> usize {
-        self.position.d_col as usize
-    }
-
-    fn turn(&mut self, new_dir: Turn) {
-        self.direction = self.direction.turn(new_dir);
-        self.straight_count = 0;
-    }
-
-    fn split(&mut self) -> Vec<Self> {
-        let left = Self {
-            direction: self.direction.turn(Turn::Left),
+    #[inline]
+    fn copy(&self, turn: Turn) -> Self {
+        Self {
+            direction: self.direction.turn(turn),
             position: self.position,
             visited_positions: self.visited_positions.clone(),
             has_cycle: self.has_cycle,
             straight_count: 0,
             length: self.length,
-        };
+        }
+    }
 
-        if self.straight_count == 2 {
+    fn advance(&mut self) {
+        let new_pos = self.position + self.direction;
+        self.has_cycle |= !self.visited_positions.insert(new_pos);
+        self.position = new_pos;
+        self.straight_count += 1;
+    }
+
+    #[inline]
+    fn in_bounds(&self) -> bool {
+        self.position.dr >= 0 && self.position.dc >= 0
+    }
+
+    #[inline]
+    fn row(&self) -> usize {
+        self.position.dr as usize
+    }
+
+    #[inline]
+    fn col(&self) -> usize {
+        self.position.dc as usize
+    }
+
+    #[inline]
+    fn turn(&mut self, new_dir: Turn) {
+        self.direction = self.direction.turn(new_dir);
+        self.straight_count = 0;
+    }
+
+    #[inline]
+    fn split(&mut self) -> Vec<Self> {
+        let left = self.copy(Turn::Left);
+
+        if self.straight_count == 3 {
             self.turn(Turn::Right);
-            return vec![left];
+            vec![left]
         } else {
-            let right = Self {
-                direction: self.direction.turn(Turn::Right),
-                position: self.position,
-                visited_positions: self.visited_positions.clone(),
-                has_cycle: self.has_cycle,
-                straight_count: 0,
-                length: self.length,
-            };
-            return vec![left, right];
+            vec![left, self.copy(Turn::Right)]
         }
     }
 }
 
-fn find_hottest_path(first_path: Path, city_grid: &[Vec<u8>], rows: usize, cols: usize) -> usize {
-    let mut paths = vec![first_path];
-    let other_paths = paths[0].split();
-    paths.extend(other_paths);
+fn find_hottest_path(mut first_path: Path, city_grid: &[Vec<usize>]) -> usize {
+    let (rows, cols) = (city_grid.len(), city_grid[0].len());
+    let goal = Vector {
+        dr: rows as isize - 1,
+        dc: cols as isize - 1,
+    };
 
-    let mut best_points: HashMap<(usize, usize), Path> = HashMap::new();
+    let mut paths = Vec::with_capacity(rows * cols / 2);
+    let mut new_paths = Vec::with_capacity(rows * cols / 2);
+    let mut best_paths_per_point: HashMap<_, (Path, Option<Path>)> =
+        HashMap::with_capacity(rows * cols / 2);
 
-    let mut min_path_length = usize::MAX;
-    let mut count = 0;
+    let mut min_path_length: Option<usize> = None;
+    // // TODO: remove
+    // let mut last_min = usize::MAX;
+
+    // first basic iteration
+    paths.push(first_path.clone());
+    paths.extend(first_path.split());
 
     while !paths.is_empty() {
-        println!("{:?}\n  {:?}", count, paths.len());
+        // print!("{:?} -- {:?}", count, paths.len());
 
         // advance beams
-        paths.iter_mut().for_each(|b| b.advance());
+        // paths.iter_mut().for_each(|b| b.advance());
+        best_paths_per_point.clear();
+
+        paths.retain_mut(|p| {
+            p.advance();
+
+            // check if we retain
+            if !p.has_cycle && p.row() < rows && p.col() < cols && p.in_bounds() {
+                // we retain, meaning we can also update be path
+                p.length += city_grid[p.row()][p.col()];
+
+                if let Some(entry) = best_paths_per_point.get_mut(&(p.row(), p.col())) {
+                    if p.length < entry.0.length {
+                        entry.1 = Some(entry.0.clone());
+                        entry.0 = p.clone();
+                    } else if entry.1.is_none() || p.length < entry.1.as_ref().unwrap().length {
+                        entry.1 = Some(p.clone());
+                    }
+                } else {
+                    best_paths_per_point.insert((p.row(), p.col()), (p.clone(), None));
+                }
+
+                true
+            } else {
+                false
+            }
+        });
 
         // filter out beams that are out of bounds
-        paths.retain(|b| b.in_bounds() && b.row() < rows && b.col() < cols && !b.has_cycle);
+        // paths.retain(|b| );
 
-        let mut new_beams = vec![];
         // interact with environment
-        best_points.clear();
-        paths.iter_mut().for_each(|b| {
-            b.length += city_grid[b.row()][b.col()] as usize;
+        // best_paths_per_point.clear();
 
-            if let Some(entry) = best_points.get_mut(&(b.row(), b.col())) {
-                if b.length < entry.length {
-                    *entry = b.clone();
-                }
-            } else {
-                best_points.insert((b.row(), b.col()), b.clone());
-            }
-        });
+        // paths.iter_mut().for_each(|b| {
+        // });
 
-        paths.retain(|b| best_points.values().any(|v| *v == *b));
+        new_paths.clear();
 
-        // for r in 0..rows {
-        //     for c in 0..cols {
-        //         if best_points.contains_key(&(r, c)) {
-        //             print!("*");
-        //         } else {
-        //             print!("_");
-        //         }
-        //     }
-        //     println!();
-        // }
-
-        paths.iter_mut().for_each(|b| {
-            if b.row() == rows - 1 && b.col() == cols - 1 {
-                if b.length < min_path_length {
-                    // TODO: print min path to end for checking
-                }
-                min_path_length = min_path_length.min(b.length);
-
-                for r in 0..rows {
-                    for c in 0..cols {
-                        if best_points.contains_key(&(r, c)) {
-                            print!("*");
-                        } else {
-                            print!("_");
-                        }
+        paths.retain_mut(|p| {
+            if best_paths_per_point
+                .values()
+                .any(|v| v.0 == *p || (v.1.is_some() && v.1.as_ref().unwrap() == p))
+            {
+                if p.position == goal {
+                    if let Some(min) = min_path_length {
+                        min_path_length = Some(min.min(p.length));
+                    } else {
+                        min_path_length = Some(p.length);
                     }
+
                     println!();
+
+                    for r in 0..rows {
+                        for c in 0..cols {
+                            if p.visited_positions.contains(&Vector {
+                                dr: r as isize,
+                                dc: c as isize,
+                            }) {
+                                print!("x");
+                            } else {
+                                print!("_");
+                            }
+                        }
+                        println!();
+                    }
+
+                    false
+                } else {
+                    new_paths.extend(p.split());
+                    true
                 }
-                path_lengths.push(b.length);
             } else {
-                new_beams.extend(b.split());
+                false
             }
         });
 
-        if let Some(min) = path_lengths.iter().min() {
-            println!("  {min:?}");
-        }
+        // paths.retain_mut(|p| {});
+
+        // TODO: remove
+        println!("{:?}", min_path_length);
+
         // combine new beams with our list
-        paths.extend(new_beams);
+        paths.extend_from_slice(&new_paths);
 
         // retain only the shortest path to each point with a path on it
-        count += 1;
+        // count += 1;
     }
 
-    *path_lengths.iter().min().unwrap()
+    // *path_lengths.iter().min().unwrap()
+    min_path_length.unwrap()
 }
 
 fn part1(filename: &str) -> usize {
     let file = std::fs::read_to_string(filename).unwrap();
-    let city_grid: Vec<Vec<u8>> = file
+    let city_grid: Vec<Vec<usize>> = file
         .split_ascii_whitespace()
         .map(|line| {
             line.chars()
-                .map(|c| c.to_digit(10).unwrap() as u8)
+                .map(|c| c.to_digit(10).unwrap() as usize)
                 .collect()
         })
         .collect();
-    let (rows, cols) = (city_grid.len(), city_grid[0].len());
 
-    let first_beam = Path::new(Vector { d_row: 0, d_col: 0 }, RIGHT);
+    let first_path = Path::new(RIGHT, Vector { dr: 0, dc: 0 });
 
-    city_grid.iter().for_each(|l| {
-        l.iter().for_each(|n| print!("{n:?}"));
-        println!();
-    });
+    // city_grid.iter().for_each(|l| {
+    //     l.iter().for_each(|n| print!("{n:?}"));
+    //     println!();
+    // });
 
-    find_hottest_path(first_beam, &city_grid, rows, cols)
+    find_hottest_path(first_path, &city_grid)
 }
 
 fn part2(filename: &str) -> usize {
