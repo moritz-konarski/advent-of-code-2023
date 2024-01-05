1a3f13d2705bafb7fc8b2ae76fded70300da61c3
Moritz Konarski <moritz.konarski@gmail.com>
2024-01-05 13:47:19 +0100

    feat: set up part 1 functions

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -2,6 +2,10 @@ use std::env;
 
 const PART1_FILE: &str = "part1.txt";
 const PART2_FILE: &str = "part2.txt";
+const UP: &str = "U";
+const DOWN: &str = "D";
+const LEFT: &str = "L";
+const RIGHT: &str = "R";
 
 fn main() {
     let usage = "Incorrect arguements!\nUsage: day-18 p<n>";
@@ -22,9 +26,45 @@ fn main() {
     }
 }
 
+fn dig_trench(commands: &[(&str, usize)]) -> Vec<(usize, usize)> {
+    let mut current = (0, 0);
+    let mut map = vec![current];
+
+    for command in commands {
+        let (direction, distance) = command;
+
+        match *direction {
+            LEFT => current.0 -= distance,
+            RIGHT => current.0 += distance,
+            UP => current.1 -= distance,
+            DOWN => current.1 += distance,
+            _ => unreachable!("illegal direction {direction}"),
+        }
+    }
+
+    map
+}
+
+fn dig_interior(trench: &[(usize, usize)]) -> usize {
+    0
+}
+
 fn part1(filename: &str) -> usize {
     let file = std::fs::read_to_string(filename).unwrap();
-    0
+    let lines = file.split_ascii_whitespace().collect::<Vec<_>>();
+    let commands = lines
+        .chunks_exact(3)
+        .map(|chunk| {
+            let distance = chunk[1].parse::<usize>().unwrap();
+            (chunk[0], distance)
+        })
+        .collect::<Vec<_>>();
+
+    let trench = dig_trench(&commands);
+
+    let volume = dig_interior(&trench);
+
+    volume
 }
 
 fn part2(filename: &str) -> usize {
